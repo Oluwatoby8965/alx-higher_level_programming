719db838bffce783e59e84db543336d77604d521
Oluwatobi Amoniyan <102077396+Oluwatoby8965@users.noreply.github.com>
2022-09-23 00:31:19 +0100

    Import Module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
+#!/usr/bin/python3.4
+if __name__ == '__main__':
+    from sys import argv
 
-    total = len(sys.argv)
-    if total <= 1:
-        print("0 arguments.")
-    else:
-        if total == 2:
-            print("{:d} argument:".format(total - 1))
-        else:
-            print("{:d} arguments:".format(total - 1))
-        for i in range(1, total):
-            print("{:d}: {}".format(i, sys.argv[i]))
+    if len(argv) == 1:
+        print('0 arguments.')
+    elif len(argv) == 2:
+        print('1 argument:')
+        print('1: {:s}'.format(argv[1]))
+    elif len(argv) > 2:
+        print('{:d} arguments:'.format(len(argv) - 1))
+        for arg in range(1, len(argv)):
+            print('{:d}: {:s}'.format(arg, argv[arg]))
