3c9c566f3793a1f201d191401750e50bc52c477e
Oluwatobi Amoniyan <102077396+Oluwatoby8965@users.noreply.github.com>
2022-10-11 15:22:55 +0100

    Classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Defines the square class"""
-
-
 class Square:
-    """Square class. Has a size"""
+    """
+    Add in the class a private instance attribute, instantiation and
+    conditions to raise errors in the execution of the code
+    """
 
-    def __init__(self, size=0):
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size =
+    def __init__(self, __size=0):
+        self.__size = __size
+        if type(__size) is not int:
+            raise TypeError('size must be an integer')
+        if __size < 0:
+            raise ValueError('size must be >= 0')
