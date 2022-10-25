9437146b9f3921ed45fab404a5f4af8fd6700525
Oluwatobi Amoniyan <102077396+Oluwatoby8965@users.noreply.github.com>
2022-10-25 16:31:53 +0100

    Input & output

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""Makes a list from args and saves them to a file in json format"""
-import sys
-import json
+"""Module 9-student
+a class Student that defines a student
+"""
 
-savejson = __import__("7-save_to_json_file").save_to_json_file
-loadjson = __import__("8-load_from_json_file").load_from_json_file
 
-try:
-    listy = loadjson("add_item.json")
-except FileNotFoundError:
-    listy = []
-for arg in sys.argv[1:]:
-    listy.append(arg)
-savejson(listy, "add_item.json")
+class Student:
+    """Defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Define instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation"""
+        return self
