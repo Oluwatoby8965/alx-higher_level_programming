5a0a6dbcf3d4bab979bb1fe9955a6ef294bba38c
Oluwatobi Amoniyan <102077396+Oluwatoby8965@users.noreply.github.com>
2022-10-25 16:51:12 +0100

    Input & output

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Defines a function that saves an object to a file as json string"""
+"""Module 5-save_to_json_file
+writes an Object to a text file
+"""
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    with open(filename, "w") as f:
-        json.dump(my_obj, f)
+    """Writes an Object to a text file, using a JSON representation"""
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(json.dumps(my_obj))
