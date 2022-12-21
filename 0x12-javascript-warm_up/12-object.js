2ea36b49eeb463a33a740ff90386d2bc233da2fd
Oluwatobi Amoniyan <102077396+Oluwatoby8965@users.noreply.github.com>
2022-12-21 23:26:17 +0100

    fx

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-
-const num1 = parseInt(process.argv[2]);
-const num2 = parseInt(process.argv[3]);
-
-console.log(num1 + num2);
+const myObject = {
+    type: 'object',
+    value: 12
+  };
+  console.log(myObject);
+  myObject.value = 89;
+  console.log(myObject);
+  
