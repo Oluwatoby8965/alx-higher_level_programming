3620137cc8adf24af792aa825f153f26005f94a3
Oluwatobi Amoniyan <102077396+Oluwatoby8965@users.noreply.github.com>
2022-12-21 22:38:44 +0100

    fix

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
-exports.nbOccurences = function (list, searchElement) {
-    const number = list.filter(item => item === searchElement).length;
-    return number;
+
+module.exports.nbOccurences = function (list, searchElement) {
+    let count = 0;
+    for (let ele in list) {
+      if (list[ele] === searchElement) {
+        count++;
+      }
+    }
+    return count;
   };
