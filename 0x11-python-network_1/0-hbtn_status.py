f14f8684dd331bc76b17488220903831ca167d8c
Oluwatobi Amoniyan <102077396+Oluwatoby8965@users.noreply.github.com>
2023-01-17 16:25:48 +0100

    fix task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""Fetch web response
-"""
-
+""" Fetching URLs with urllib """
 import urllib.request
 
 if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        content = response.read()
-    print('Body response:')
-    print('\t- type: {}'.format(type(content)))
-    print('\t- content: {}'.format(content))
-    print('\t- utf8 content: {}'.format(str(content)[2:-1]))
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+              .format(type(html), html, html.decode('utf-8')))
